9ce019c8c0dd459ed21d74ea99131e38eb5e1734
Piotr Pasza Storozenko <piotr@appsilon.com>
2024-04-05 16:04:41 +0200

    refactor: move fonts import to css file

--- app.py
+++ app.py
@@ -12,15 +12,8 @@ app_settings = AppSettings()
 logger.remove()
 logger.add(sys.stderr, level=app_settings.log_level)
 
-# External resources, like CSS and JS files
-# Can be moved to a separate file
-google_fonts_tag = ui.TagList(
-    ui.tags.link(rel="stylesheet", href="https://fonts.googleapis.com/css2?family=Roboto"),
-    ui.tags.link(rel="stylesheet", href="https://fonts.googleapis.com/css2?family=Maven+Pro"),
-)
-
 # Combine clean shiny UI with CSS and external resources
-ui_with_css = ui.TagList(ui.tags.link(href="style.css", rel="stylesheet"), google_fonts_tag, get_dashboard_ui())
+ui_with_css = ui.TagList(ui.tags.link(href="style.css", rel="stylesheet"), get_dashboard_ui())
 
 app_dir = Path(__file__).parent
 app = App(ui_with_css, server, static_assets=app_dir / "www")
