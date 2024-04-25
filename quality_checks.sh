cacae6c1b8c8a1e638e31cd0bf79433c28d9f601
Piotr Pasza Storozenko <piotr@appsilon.com>
2024-04-25 12:54:29 +0200

    feat: swap mypy for pyright

--- quality_checks.sh
+++ quality_checks.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # This script is for running quality checks in GitHub Actions.
-# It runs tests, and type checks with mypy.
+# It runs tests, and type checks with pyright.
 
 set -e # Exit immediately if a command exits with a non-zero status.
 
 echo "Running pytest for unit tests..."
 poetry run pytest
 
-echo "Running mypy for type checking..."
-poetry run mypy tapyr_template
+echo "Running pyright for type checking..."
+poetry run pyright tapyr_template
